72527b1624671b86e587a4ac1abeed68cfa815ce
dahoo <dahoo@DESKTOP-SE8G5DJ>
2025-08-25 16:08:06 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿string name = "Dan Chung";
+int age = 28;
+string addresse = "80 Fawcett st. Cambridge, Massachusetts 02138";
+
+Console.WriteLine($"User's name is: {name}");
+Console.WriteLine($"User's age is: {age}");
+Console.WriteLine($"User's address is: {addresse}");
